d714914b9b5b17c49413912ef9891f9171c01c62
akbarasadov <akbarasadov4170@gmail.com>
2025-02-18 21:57:29 +0500

    first upload

---
+++ js.js
@@ -0,0 +1,17 @@
+//1
+let a = 0
+for (let i = 1; i <= 100; i++) {
+    let num = a=a+i
+    console.log(num);
+}
+
+//2
+let names = ["Илья", "Амаль", "Ахмад", "Фаррух", "Шерик"]
+for (let b = 0; b < names.length; b++) {
+   console.log(`Привет, ${names[b]}`)
+}
+// 3
+let text = "Text"
+for (let c = 0; c < text.length; c++) {
+    console.log(text[c]);
+}
